e4f92c165732d7bc1a4cb3003fe4419f4c13d5f7
Shinsuke <sakai@fml.t.u-tokyo.ac.jp>
2020-12-09 14:27:55 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+setup(
+    name="Utility",
+    version="1.0.0",
+    author="Shinsuke Sakai",
+    url="https://github/ShinsukeSakai0321/Utility.git",
+    packages=find_packages("src"),
+    package_dir={"":"src"},
+    package_data={'':['*.csv']},
+    include_package_data=True,
+)
